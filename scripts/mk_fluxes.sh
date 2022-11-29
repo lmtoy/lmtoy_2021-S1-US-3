a4dd8f8b29b9a2466afde549665601985955189f
Peter Teuben <teuben@gmail.com>
2022-11-29 12:54:46 -0500

    upd

--- scripts/mk_fluxes.sh
+++ scripts/mk_fluxes.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 #
-#
+# M100_Feather_CO.image.mom0.pbcor.fits
+# M100_combine_CO_cube.image.mom0.pbcor.fits
+# M100_LMT.mom0.fits 
+
+# @todo the ALMA maps don't have the same resolution as the LMT maps
+#       4.6 x 2.9"   vs.  12.65"
 
 fitsccd M100_Feather_CO.image.mom0.pbcor.fits - |\
  ccdellint - 0:200/3600:1/3600 150 40 180,263 tab=- rscale=3600 iscale=1/60.49 > M100_Feather.cumflux.tab
@@ -14,6 +19,7 @@ fitsccd M100_combine_CO_cube.image.mom0.pbcor.fits - |\
 # cd ~/ALMA/DataComb/data/2021-S1-US-3/97520_99703
 # Jy/K ~ 2.16 (= 1.41 / 0.65)
 # 1.24 is the factor to bring it to ALMA
+# @todo this is still the wrong conversion factor
 fitsccd M100_LMT.mom0.fits  - |\
    ccdellint - 0:240/3600:10/3600 150 40 45,45 tab=- rscale=3600 iscale=1.41/0.55/0.65/6.0 >  M100_LMT.cumflux.tab
 
