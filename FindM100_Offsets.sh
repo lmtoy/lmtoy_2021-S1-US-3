be69558ab7f4bf1952676f9dd1fd8b6ec10be081
Peter Teuben <teuben@gmail.com>
2025-06-03 17:19:28 -0400

    merged

--- FindM100_Offsets.sh
+++ FindM100_Offsets.sh
@@ -2,17 +2,25 @@
 #
 #    find centers of mom0 maps using NEMO's ccdblob
 #
+#    disturbing:
+# ./FindM100_Offsets.sh box=4 > tab4
+# ./FindM100_Offsets.sh box=3 > tab3
+# paste tab3 tab4 | tabplot - 3 7 41 43 41 43 point=2,0.1 layout=diag.layout
+#    this is not a 1:1 relationship
+
+# the 39 good obsnums for M100
 obsnums=97520,97521,97523,97524,97528,97529,97531,97532,97861,97863,97864,\
 97870,97871,97993,97994,97999,98000,98006,98011,98012,98686,98687,\
 98691,98692,98726,98727,98731,98732,98736,98737,98974,98975,99674,\
 99675,99679,99680,99702,99703
 
+# 
 pos=41,41
 box=5
 link=2021-S1-US-3
 link=data_2023
 link=data_2025
-clip=0
+clip=20
 
 #       parse the commandline
 for arg in "$@"; do
@@ -22,7 +30,6 @@ done
 
 
 echo '# Computed with FindM100_Offsets.sh '
-echo "# pos=$pos box=$box clip=$clip link=$link"
 echo '#'
 echo '# Obsnum   XMEAN    YMEAN'
 
@@ -31,9 +38,20 @@ export DEBUG=-1
 for o in $(nemoinp $obsnums); do
     file=$link/${o}/M100_${o}__0.mom0.fits
     if [ -e $file ]; then
-	cen=$(fitsccd $file - | ccdblob - wcs=f pos=$pos box=$box clip=$clip | txtpar - p0=Center,1,2 p1=Center,1,3)
-	echo "$o  $cen"
+	cen1="cen1"
+	cen2="cen2"
+	c=""
+	# cen1:   a blob fit using moments of inertia, using box= and clip= to select
+	cen1=$(fitsccd $file - | ccdblob - wcs=f pos=$pos box=$box clip=$clip | txtpar - p0=Center,1,2 p1=Center,1,3 p2=DataMinMax,1,3)
+	if [ $? != 0 ] ; then  c="#" ; fi
+
+	# cen2: a 2D gaussian fit, but using ccdblob to select the same points as used in the MOI method
+	#       making sure pixel 1 based coordinates returned
+	fitsccd $file - | ccdblob - wcs=f pos=$pos box=$box clip=$clip out=- | tabcomment - > tmp_$o.tab
+	cen2=$(tabnllsqfit tmp_$o.tab  1,2 3 fit=gauss2d par=1,40,40,40,2|txtpar - %1,%2,%3+1,%4+1,%5,%6,%7 p0=a=,1,2 p1=b=,1,2 p2=c=,1,2 p3=d=,1,2 p4=e=,1,2 p5=c=,1,3 p6=d=,1,3)
+	if [ $? != 0 ] ; then  c="#" ; fi
     else
 	echo "# $o missing $file"
     fi
+    echo $c $o $cen1 $cen2
 done
